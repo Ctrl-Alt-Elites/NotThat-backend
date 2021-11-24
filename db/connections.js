7a6729c150b887067a5d4a34fc50d398200848aa
tsfong <tsfong@uci.edu>
2021-11-23 16:36:06 -0800

    updated connections

--- db/connections.js
+++ db/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://localhost/restaurants', {useNewUrlParser:true})
+mongoose.connect('mongodb://localhost/restaurants', {useNewUrlParser:true})
 
 let mongoURI = "";
 
